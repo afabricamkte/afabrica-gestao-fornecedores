0c7f00b2cbcb473947af46cf0189ae4c348db90d
afabricamkte <filipe.paula.f1@gmail.com>
2025-03-26 01:28:31 +0000

    merda

---
+++ afabrica-gestao-fornecedores/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>AFÁBRICA - Sistema de Gestão de Fornecedores</h1>
+      <p>Aplicação em construção...</p>
+    </div>
+  );
+};
+
+export default App;
